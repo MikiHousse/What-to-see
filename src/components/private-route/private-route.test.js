bfe8520f910caba0e6b2e375c28e038daaf95397
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-05-24 10:11:14 +0300

    add error correction to the private route test

--- src/components/private-route/private-route.test.js
+++ src/components/private-route/private-route.test.js
@@ -1,6 +1,6 @@
 import { createMemoryHistory } from "history";
 import React from "react";
-import { Route, Router } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import * as redux from "react-redux";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
@@ -14,7 +14,7 @@ let history;
 describe(`Private-route work correctly`, () => {
   beforeEach(() => {
     history = createMemoryHistory();
-    history.push("/private");
+    history.push(`/private`);
   });
   it(`If user no autorized he'll be redirect to /login`, () => {
     jest.spyOn(redux, `useSelector`);
@@ -33,10 +33,31 @@ describe(`Private-route work correctly`, () => {
           <PrivateRoute
             exact
             path="/private"
-            render={() => {
-              <h1>Private Route</h1>;
-            }}
+            render={() => <h1>Private Route</h1>}
           />
+        </Router>
+      </Provider>
+    );
+    expect(screen.getByText(/Public Route/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Private Route/i)).not.toBeInTheDocument();
+  });
+  it(`If user authorized he'll be redirect to /login`, () => {
+    jest.spyOn(redux, `useSelector`);
+    jest.spyOn(redux, `useDispatch`);
+    const store = mockStore({
+      USER: {
+        authorizationStatus: AuthorizationStatus.AUTH,
+      },
+    });
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Route exact path="/login">
+            <h1>Public Route</h1>
+          </Route>
+          <PrivateRoute exact path="/private">
+            <h1>Private Route</h1>
+          </PrivateRoute>
         </Router>
       </Provider>
     );
