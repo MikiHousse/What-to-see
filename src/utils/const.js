8493d862f3ac1fa1e2d6273977738fd890eac694
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-06-17 06:39:40 +0300

    add fix api

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,28 +1,28 @@
 export const AppRoute = {
-  LOGIN: '/login',
-  MAIN: '/',
-  MY_LIST: '/mylist',
-  FILM: '/films/:id',
-  REVIEW: '/films/:id/review',
-  PLAYER: '/player/:id',
-  NOT_FOUND: '/not-found'
-}
+  LOGIN: "/login",
+  MAIN: "/",
+  MY_LIST: "/mylist",
+  FILM: "/films/:id",
+  REVIEW: "/films/:id/review",
+  PLAYER: "/player/:id",
+  NOT_FOUND: "/not-found",
+};
 
 export const AuthorizationStatus = {
-  AUTH: 'AUTH',
-  NO_AUTH: 'NO_AUTH',
-  UNKNOWN: 'UNKNOWN',
-}
+  AUTH: "AUTH",
+  NO_AUTH: "NO_AUTH",
+  UNKNOWN: "UNKNOWN",
+};
 
 export const ApiRoute = {
-  FILMS: '/films',
-  PROMO: '/films/promo',
-  PLAYER: '/player',
-  FAVORITE: '/favorite',
-  REVIEW: 'review',
-  LOGIN: '/login',
-  LOGOUT: '/logout',
-  COMMENTS: '/comments',
+  FILMS: "/films",
+  PROMO: "/promo",
+  PLAYER: "/player",
+  FAVORITE: "/favorite",
+  REVIEW: "review",
+  LOGIN: "/login",
+  LOGOUT: "/logout",
+  COMMENTS: "/comments",
 };
 
 export const LIST_COUNT_FILMS = 8;
@@ -31,12 +31,12 @@ export const MORE_LIKE_COUNT = 4;
 
 export const ALL_GENRES = `All genres`;
 
-export const MAX_COUNT_GENRES = 9
+export const MAX_COUNT_GENRES = 9;
 
 export const FlagFavorite = {
   TRUE: 1,
   FALSE: 0,
-}
+};
 
 export const STARS_COUNT = 10;
 
@@ -44,5 +44,5 @@ export const FetchStatus = {
   FINALLY: `FINALLY`,
   SENDING: `SENDING`,
   DONE: `DONE`,
-  ERROR: `ERROR`
+  ERROR: `ERROR`,
 };
